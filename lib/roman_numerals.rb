2c6d694a2f74fa989e45bb4051e72fa4f00748ff
Dan Pelensky <dan@pelensky.com>
2016-11-16 10:02:57 +0000

    working except for 4s and 9s - needs lots of refactoring

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,33 +1,72 @@
-def multiple_of_1000?(number)
-  return true if number / 1000 != 0
-  return false
+def divided_by(number, divisor)
+  number / divisor
 end
 
-def multiple_of_500?(number)
-  return true if number / 500 != 0
-  return false
+def remainder(number, divisor)
+  number % divisor
 end
 
-def multiple_of_100?(number)
-  return true if number / 100 != 0
-  return false
+def divided_by_1000(number)
+  divided_by(number, 1000)
 end
 
-def multiple_of_50?(number)
-  return true if number / 50 != 0
-  return false
+def remainder_of_1000(number)
+  remainder(number, 1000)
 end
 
-def multiple_of_10?(number)
-  return true if number / 10 != 0
-  return false
+def divided_by_500(number)
+  divided_by(number, 500)
 end
 
-def multiple_of_5?(number)
-  return true if number / 5 != 0
-  return false
+def remainder_of_500(number)
+  remainder(number, 500)
 end
 
-def multiple_of_1?(number)
-  true
+def divided_by_100(number)
+  divided_by(number, 100)
+end
+
+def remainder_of_100(number)
+  remainder(number, 100)
+end
+
+def divided_by_50(number)
+  divided_by(number, 50)
+end
+
+def remainder_of_50(number)
+  remainder(number, 50)
+end
+
+def divided_by_10(number)
+  divided_by(number, 10)
+end
+
+def remainder_of_10(number)
+  remainder(number, 10)
+end
+
+def divided_by_5(number)
+  divided_by(number, 5)
+end
+
+def remainder_of_5(number)
+  remainder(number, 5)
+end
+
+def letters(number)
+  roman_thousand = "M" * divided_by_1000(number)
+  remainder = remainder_of_1000(number)
+  roman_five_hundred = "D" * divided_by_500(remainder)
+  remainder = remainder_of_500(remainder)
+  roman_hundred = "C" * divided_by_100(remainder)
+  remainder = remainder_of_100(remainder)
+  roman_fifty = "L" * divided_by_50(remainder)
+  remainder = remainder_of_50(remainder)
+  roman_ten = "X" * divided_by_10(remainder)
+  remainder = remainder_of_10(remainder)
+  roman_five = "V" * divided_by_5(remainder)
+  remainder = remainder_of_5(remainder)
+  roman_one = "I" * remainder
+  return roman_thousand + roman_five_hundred + roman_hundred + roman_fifty + roman_ten + roman_five + roman_one
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -1,64 +1,93 @@
 require "./lib/roman_numerals.rb"
 
 describe "roman_numerals" do
-  context "high level calculations" do
-    it "should check if the number is divisible by 1000" do
-      expect(multiple_of_1000?(1000)).to eq true
+
+  context "number calculations" do
+    it "should divide a number by another number" do
+      expect(divided_by(10, 5)).to eq 2
     end
 
-    it "should check if the number is not divisible by 1000" do
-      expect(multiple_of_1000?(1)).to eq false
+    it "should check for the remainder when a number is divided by another" do
+      expect(remainder(3, 2)).to eq 1
     end
 
-    it "should check if the number is divislbe by 500" do
-      expect(multiple_of_500?(500)).to eq true
+    it "should divide a number by 1000 and store the number" do
+      expect(divided_by_1000(1000)).to eq 1
     end
 
-    it "should check if the number is not divisible by 500" do
-      expect(multiple_of_500?(1)). to eq false
+    it "should check the remainder of a number that has been divided by 1000 and store that number" do
+      expect(remainder_of_1000(1)).to eq 1
     end
 
-    it "should check if the number is divisible by 100" do
-      expect(multiple_of_100?(100)).to eq true
+    it "should divide a number by 500 and store the number" do
+      expect(divided_by_500(500)).to eq 1
     end
 
-    it "should check if the number is not divisible by 100" do
-      expect(multiple_of_100?(1)).to eq false
+    it "should check the remainder of a number that has been divided by 500 and store that number" do
+      expect(remainder_of_500(1)).to eq 1
     end
 
-    it "should check if the number is divisible by 50" do
-      expect(multiple_of_50?(50)).to eq true
+    it "should divide a number by 100 and store that number" do
+      expect(divided_by_100(100)).to eq 1
     end
 
-    it "should check if the number is not divisible by 50" do
-      expect(multiple_of_50?(1)).to eq false
+    it "should check the remainder of a number that has been divided by 100 and store taht number" do
+      expect(remainder_of_100(1)).to eq 1
     end
 
-    it "should check if the number is divisible by 10" do
-      expect(multiple_of_10?(10)).to eq true
+    it "should divide a number by 50 and store that number" do
+      expect(divided_by_50(50)).to eq 1
     end
 
-    it "should check if the number is not divisble by 10" do
-      expect(multiple_of_10?(1)).to eq false
+    it "should check the remainder of a number that has been divided by 50 and store that number" do
+      expect(remainder_of_50(1)).to eq 1
     end
 
-    it "should check if the number is divisible by 5" do
-      expect(multiple_of_5?(5)).to eq true
+    it "should divide a number by 10 and store that number" do
+      expect(divided_by_10(10)).to eq 1
     end
 
-    it "should cehck if the number is not divisible by 5" do
-      expect(multiple_of_5?(1)).to eq false
+    it "should check the remainder of a number that has been divided by 10 and store that number" do
+      expect(remainder_of_10(1)).to eq 1
     end
 
-    it "should check if the number is divisible by 1" do
-      expect(multiple_of_1?(1)).to eq true
+    it "should divide a number by 5 and store that number" do
+      expect(divided_by_5(5)).to eq 1
     end
-end
 
+    it "should check the remainder of a number that has been divided by 5, and store that number" do
+      expect(remainder_of_5(1)).to eq 1
+    end
+  end
 
+  context "calculation" do
+    it "should store the letter M for multiples of 1000" do
+      expect(letters(1000)).to eq "M"
+    end
 
+    it "should store the letter D for multiples of 500" do
+      expect(letters(500)).to eq "D"
+    end
 
+    it "should store the letter C for multiples of 100" do
+      expect(letters(100)).to eq "C"
+    end
 
+    it "should store the letter L for multiples of 50" do
+      expect(letters(50)).to eq "L"
+    end
 
+    it "should store the letter X for multiples of 10" do
+      expect(letters(10)).to eq "X"
+    end
+
+    it "should store the letter V for multiples of 5" do
+      expect(letters(5)).to eq "V"
+    end
+
+    it "should store the letter I for multiples of 1" do
+      expect(letters(1)).to eq "I"
+    end
+  end
 
 end
