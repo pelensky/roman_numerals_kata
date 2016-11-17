16b3378f6de24f4b353fb19f7984a8ff96682885
Dan Pelensky <dan@pelensky.com>
2016-11-17 09:56:56 +0000

    don't really understand what I'm trying to do - not passing tests, going back to my old way for now

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,72 +1,27 @@
-def divided_by(number, divisor)
-  number / divisor
-end
+class RomanNumeral
 
-def remainder(number, divisor)
-  number % divisor
-end
+  ROMAN_NUMERALS = {
+    1000 => "M",
+    900 => "CM",
+    500 => "D",
+    400 => "CD",
+    100 => "C",
+    90 => "XC",
+    50 => "L",
+    40 => "XL",
+    10 => "X",
+    9 => "IX",
+    5 => "V",
+    4 => "IV",
+    1 => "I"
+  }
 
-def divided_by_1000(number)
-  divided_by(number, 1000)
-end
+  def converter(number)
+    result = ""
+    result << "M" * (number / 1000) 
+    number -= 1000
 
-def remainder_of_1000(number)
-  remainder(number, 1000)
-end
 
-def divided_by_500(number)
-  divided_by(number, 500)
-end
+  end
 
-def remainder_of_500(number)
-  remainder(number, 500)
-end
-
-def divided_by_100(number)
-  divided_by(number, 100)
-end
-
-def remainder_of_100(number)
-  remainder(number, 100)
-end
-
-def divided_by_50(number)
-  divided_by(number, 50)
-end
-
-def remainder_of_50(number)
-  remainder(number, 50)
-end
-
-def divided_by_10(number)
-  divided_by(number, 10)
-end
-
-def remainder_of_10(number)
-  remainder(number, 10)
-end
-
-def divided_by_5(number)
-  divided_by(number, 5)
-end
-
-def remainder_of_5(number)
-  remainder(number, 5)
-end
-
-def letters(number)
-  roman_thousand = "M" * divided_by_1000(number)
-  remainder = remainder_of_1000(number)
-  roman_five_hundred = "D" * divided_by_500(remainder)
-  remainder = remainder_of_500(remainder)
-  roman_hundred = "C" * divided_by_100(remainder)
-  remainder = remainder_of_100(remainder)
-  roman_fifty = "L" * divided_by_50(remainder)
-  remainder = remainder_of_50(remainder)
-  roman_ten = "X" * divided_by_10(remainder)
-  remainder = remainder_of_10(remainder)
-  roman_five = "V" * divided_by_5(remainder)
-  remainder = remainder_of_5(remainder)
-  roman_one = "I" * remainder
-  return roman_thousand + roman_five_hundred + roman_hundred + roman_fifty + roman_ten + roman_five + roman_one
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -1,93 +1,18 @@
 require "./lib/roman_numerals.rb"
 
-describe "roman_numerals" do
+describe RomanNumeral do
 
-  context "number calculations" do
-    it "should divide a number by another number" do
-      expect(divided_by(10, 5)).to eq 2
-    end
+  subject (:romannumeral) {described_class.new}
 
-    it "should check for the remainder when a number is divided by another" do
-      expect(remainder(3, 2)).to eq 1
-    end
-
-    it "should divide a number by 1000 and store the number" do
-      expect(divided_by_1000(1000)).to eq 1
-    end
-
-    it "should check the remainder of a number that has been divided by 1000 and store that number" do
-      expect(remainder_of_1000(1)).to eq 1
-    end
-
-    it "should divide a number by 500 and store the number" do
-      expect(divided_by_500(500)).to eq 1
-    end
-
-    it "should check the remainder of a number that has been divided by 500 and store that number" do
-      expect(remainder_of_500(1)).to eq 1
-    end
-
-    it "should divide a number by 100 and store that number" do
-      expect(divided_by_100(100)).to eq 1
-    end
-
-    it "should check the remainder of a number that has been divided by 100 and store taht number" do
-      expect(remainder_of_100(1)).to eq 1
-    end
-
-    it "should divide a number by 50 and store that number" do
-      expect(divided_by_50(50)).to eq 1
-    end
-
-    it "should check the remainder of a number that has been divided by 50 and store that number" do
-      expect(remainder_of_50(1)).to eq 1
-    end
-
-    it "should divide a number by 10 and store that number" do
-      expect(divided_by_10(10)).to eq 1
-    end
-
-    it "should check the remainder of a number that has been divided by 10 and store that number" do
-      expect(remainder_of_10(1)).to eq 1
-    end
-
-    it "should divide a number by 5 and store that number" do
-      expect(divided_by_5(5)).to eq 1
-    end
-
-    it "should check the remainder of a number that has been divided by 5, and store that number" do
-      expect(remainder_of_5(1)).to eq 1
-    end
+  it "expects 1000 to equal M" do
+    expect(romannumeral.converter(1000).result).to eq("M")
   end
 
-  context "calculation" do
-    it "should store the letter M for multiples of 1000" do
-      expect(letters(1000)).to eq "M"
-    end
-
-    it "should store the letter D for multiples of 500" do
-      expect(letters(500)).to eq "D"
-    end
-
-    it "should store the letter C for multiples of 100" do
-      expect(letters(100)).to eq "C"
-    end
-
-    it "should store the letter L for multiples of 50" do
-      expect(letters(50)).to eq "L"
-    end
-
-    it "should store the letter X for multiples of 10" do
-      expect(letters(10)).to eq "X"
-    end
-
-    it "should store the letter V for multiples of 5" do
-      expect(letters(5)).to eq "V"
-    end
-
-    it "should store the letter I for multiples of 1" do
-      expect(letters(1)).to eq "I"
-    end
+  it "expects 2000 to equal MM" do
+    expect(romannumeral.converter(2000).result).to eq("MM")
   end
 
+  # it "expects 4 to equal IV" do
+  #   expect(romannumeral.converter(4)).to eq("IV")
+  # end
 end
